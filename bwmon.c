98ca1e994e4cc39b55f4f7ba4aab9dbcd3af02b5
Mike Hayes <mike@nanobit.net>
2017-02-18 15:25:27 -0600

    Add files via upload
    Code cleanup / Warning nullify.

--- bwmon.c
+++ bwmon.c
@@ -37,183 +37,163 @@ static unsigned long int bsentrem;
 static unsigned long int brecrem;
 float _brec = 0.00;
 float _bsent = 0.00;
-int fetchline(char s[], int lim, FILE * fp);
-
-void print_cmd_error(void)
+int fetchline (char s[], int lim, FILE * fp);
+void sleep (int x);
+void
+print_cmd_error (void)
 {
-    printf("Usage: bwmon <adapter> <1000 or 1024 (for mb/kb accounting)>\r\n");
-    return;
+  printf ("Usage: bwmon <adapter> <1000 or 1024 (for mb/kb accounting)>\r\n");
+  return;
 }
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-    struct tm che;
-    time_t thetime;
-    long int old_time;
-    FILE *fp;
-    char adapt[1024];
-    char file[1024];
-    char type[2];
-    char rtype[2];
-    float fsent, frecv;
-    int mult;
-    int i;
-    
-    thetime = time(NULL);
-    
-    localtime_r(&thetime, &che);
-    old_time = 100;
+  struct tm che;
+  time_t thetime;
+  long int old_time;
+  FILE *fp;
+  char adapt[1024];
+  char file[1024];
+  char type[2];
+  char rtype[2];
+  float fsent, frecv;
+  int mult;
+  int i;
+  int skip = 0;			//For skipping the tokens below.
 
-    fsent = frecv = 0.0f;
-    type[0] = rtype[0] = file[0] = adapt[0] = '\0';
-    mult = 1024;
-    if (argc <= 1)
-        print_cmd_error();
+  thetime = time (NULL);
 
-    for (i = 0; i < argc; i++) {
-        printf("Arguments: %d) %s\r\n", i, argv[i]);
+  localtime_r (&thetime, &che);
+  old_time = 100;
+
+  fsent = frecv = 0.0f;
+  type[0] = rtype[0] = file[0] = adapt[0] = '\0';
+  mult = 1024;
+  if (argc <= 1)
+    print_cmd_error ();
+
+  for (i = 0; i < argc; i++)
+    {
+      printf ("Arguments: %d) %s\r\n", i, argv[i]);
     }
 
-    if (argv[1])
-        sprintf(adapt, "%s", argv[1]);
-    if (argc > 2 && argv[2])
-        mult = atoi(argv[2]);
+  if (argv[1])
+    sprintf (adapt, "%s", argv[1]);
+  if (argc > 2 && argv[2])
+    mult = atoi (argv[2]);
 
-    if (mult <= 0)
-        mult = 0;
-    if (mult != 1024 && mult != 1000) {
-        printf("Second argument must be 1000 or 1024.\r\n");
-        return;
+  if (mult <= 0)
+    mult = 0;
+  if (mult != 1024 && mult != 1000)
+    {
+      printf ("Second argument must be 1000 or 1024.\r\n");
+      return 0;
     }
 
-    printf("Beginning.\n\r\n\r");
+  printf ("Beginning.\n\r\n\r");
 
-    while (old_time > -10) {
-        localtime_r(&thetime, &che);
-        
-        sleep(1); // Sleep for a bit before updating.
-        
-        old_time = che.tm_sec;
-        
-        if ((fp = fopen("/proc/net/dev", "r")) == NULL) {
-            printf("Unable to open proc file!\n\n");
-            return 0;
-        }
+  while (old_time > -10)
+    {
+      localtime_r (&thetime, &che);
 
-        while (!feof(fp)) {
+      sleep (1);		// Sleep for a bit before updating.
 
-            unsigned char bline[500];
+      old_time = che.tm_sec;
 
-            fetchline(bline, 1000, fp);
-            if (strstr(bline, adapt[0] == '\0' ? "eth0:" : adapt)) {
-                char deli[] = " ";
-                char *token, *buf;
+      if ((fp = fopen ("/proc/net/dev", "r")) == NULL)
+	{
+	  printf ("Unable to open proc file!\n\n");
+	  return 0;
+	}
 
-                buf = strdup(&bline[7]);
+      while (!feof (fp))
+	{
 
-                token = strtok(buf, deli);      // bytes (rec)
-                brec = strtof(token, NULL);
+	  unsigned char bline[500];
 
-                token = strtok(NULL, deli);     // packets
-                token = strtok(NULL, deli);     // errs
-                token = strtok(NULL, deli);     // drop
-                token = strtok(NULL, deli);     // fifo
-                token = strtok(NULL, deli);     // frame
-                token = strtok(NULL, deli);     // compressed
-                token = strtok(NULL, deli);     // multicast
-                token = strtok(NULL, deli);     // bytes (send)
-                bsent = strtof(token, NULL);
-                token = strtok(NULL, deli);     // packets
-                token = strtok(NULL, deli);     // errs
-                token = strtok(NULL, deli);     // drop
-                token = strtok(NULL, deli);     // fifo
-                token = strtok(NULL, deli);     // frame
-                token = strtok(NULL, deli);     // compressed
-                token = strtok(NULL, deli);     // multicast
+	  fetchline (bline, 1000, fp);
+	  if (strstr (bline, adapt[0] == '\0' ? "eth0:" : adapt))
+	    {
+	      char deli[] = " ";
+	      char *token, *buf;
 
-                _brec = brec - _brec;
-                _bsent = bsent - _bsent;
+	      buf = strdup (&bline[7]);
 
-                fsent = (float)_bsent;
-                frecv = (float)_brec;
+	      token = strtok (buf, deli);	// bytes (rec)
+	      brec = strtof (token, NULL);
 
-                type[0] = 'b';
-                type[1] = '\0';
-                rtype[0] = 'b';
-                rtype[1] = '\0';
+	      for (skip = 0; skip < 8; skip++)
+		token = strtok (NULL, deli);	// Skip 7 tokens
 
-                if (fsent > mult) {
-                    fsent = fsent / mult;
-                    type[0] = 'K';
-                    type[1] = '\0';
-                }
-                if (fsent > mult) {
-                    fsent = fsent / mult;
-                    type[0] = 'M';
-                    type[1] = '\0';
-                }
-                if (fsent > mult) {
+	      bsent = strtof (token, NULL);
 
-                    fsent = fsent / mult;
-                    type[0] = 'G';
-                    type[1] = '\0';
-                }
+	      for (skip = 0; skip < 6; skip++)
+		token = strtok (NULL, deli);	// Skip 6 tokens.
 
-                if (frecv > mult) {
+	      _brec = brec - _brec;
+	      _bsent = bsent - _bsent;
 
-                    frecv = frecv / mult;
-                    rtype[0] = 'K';
-                    rtype[1] = '\0';
-                }
+	      fsent = (float) _bsent;
+	      frecv = (float) _brec;
 
-                if (frecv > mult) {
+	      type[0] = rtype[0] = 'b';
+	      type[1] = rtype[1] = '\0';
+	      /* Yeah, these if-statements break every code formatting rule in the world
+	       * but I do not really care right now. It saves space for such a small
+	       * program. 
+	       * -M/h
+	       */
+	      
+	      if (fsent > mult) {fsent = fsent / mult; type[0] = 'K';}
+	      if (fsent > mult) {fsent = fsent / mult; type[0] = 'M';}
+	      if (fsent > mult) {fsent = fsent / mult; type[0] = 'G';}
+	      if (frecv > mult)	{frecv = frecv / mult;rtype[0] = 'K';}
+	      if (frecv > mult) {frecv = frecv / mult;rtype[0] = 'M';}
+	      if (frecv > mult)	{frecv = frecv / mult;rtype[0] = 'G';}
 
-                    frecv = frecv / mult;
-                    rtype[0] = 'M';
-                    rtype[1] = '\0';
-                }
+	      rtype[1] = '\0';
 
-                if (frecv > mult) {
+	      printf ("\e[1A\e[2K");
 
-                    frecv = frecv / mult;
-                    rtype[0] = 'G';
-                    rtype[1] = '\0';
-                }
+	      printf ("Send: %.3f%s/s - Rec: %.3f%s/s\n", fsent, type, frecv,
+		      rtype);
 
-                printf("\e[1A\e[2K");
+	      _bsent = bsent;
+	      _brec = brec;
+	      bsentrem = brecrem = 0;
 
-                printf("Send: %.3f%s/s - Rec: %.3f%s/s\n", fsent, type, frecv, rtype);
+	    }
 
-                _bsent = bsent;
-                _brec = brec;
-                bsentrem = 0;
-                brecrem = 0;
-
-            }
-
-        }
-        fclose(fp);
+	}
+      fclose (fp);
 
     }
 
-    return 0;
+  return 0;
 }
+
 // Fetchline: reads one line out of the given file and returns with that data.
 // Will fail gracefully if nothing is there to return.
-int fetchline(char s[], int lim, FILE * fp)
+int
+fetchline (char s[], int lim, FILE * fp)
 {
-    int c, i, j;
-    for (i = 0, j = 0; (c = getc(fp)) != EOF && c != '\n'; ++i) {
-        if (i < lim - 1) {
-            s[j++] = c;
-        }
+  int c, i, j;
+  for (i = 0, j = 0; (c = getc (fp)) != EOF && c != '\n'; ++i)
+    {
+      if (i < lim - 1)
+	{
+	  s[j++] = c;
+	}
     }
-    if (c == '\n') {
-        if (i <= lim - 1) {
-            s[j++] = c;
-        }
-        ++i;
+  if (c == '\n')
+    {
+      if (i <= lim - 1)
+	{
+	  s[j++] = c;
+	}
+      ++i;
     }
-    s[j] = '\0';
-    return i;
+  s[j] = '\0';
+  return i;
 }
